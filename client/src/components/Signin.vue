9f6fa21f84fb55954fc24576ac7ed6dc8cdb1457
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-06-15 15:13:18 +0700

    menambahkan components signin

---
+++ client/src/components/Signin.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+  <div class="ui form" @submit.prevent="submitSignin">
+    <div class="field">
+      <label>Username</label>
+      <input type="text" name="username" placeholder="Username..." v-model="userForm.username">
+    </div>
+    <div class="field">
+      <label>Password</label>
+      <input type="text" name="password" placeholder="Password..." v-model='userForm.password'>
+    </div>
+    <button type="button" name="button" class="ui button">Log In</button>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import {mapGetters, mapActions} from 'vuex'
+
+  export default{
+    data() {
+      userForm: {
+        username: '',
+        password: ''
+      }
+    },
+    methods: {
+      ...mapactions([
+        'userSignin'
+      ]),
+      submitSignin() {
+        console.log("submitSignin");
+        console.log(this.userForm);
+
+        this.userSignin(this.userForm)
+        this.$router.push('/')
+      }
+    }
+  }
+</script>
+
+<style media="screen">
+
+</style>
